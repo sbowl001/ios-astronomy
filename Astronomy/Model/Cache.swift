720d4241f087a77cce8d98607e8affe2086cf1b7
Stephanie Bowles <sbowl001@fiu.edu>
2019-08-15 00:16:02 +0800

    updates to caching

--- Astronomy/Model/Cache.swift
+++ Astronomy/Model/Cache.swift
@@ -10,11 +10,18 @@ import Foundation
 
 class Cache<Key: Hashable, Value> {
     private var cachedItems: [Key: Value] = [:]
+    private let queue = DispatchQueue(label: "SerialQueue")
 //    Create a private property that is a dictionary to be used to actually store the cached items. The type of the dictionary should be [Key : Value]. Make sure you initialize it with an empty dictionary.
-    func cache(value:Value, for key: Key) {
-//        queue.sync {
+    func cache(value: Value, for key: Key) {
+        queue.sync {
             self.cachedItems[key] = value
-//        }
+        }
+    }
+    
+    func value(key: Key) -> Value? {
+        return queue.sync {
+            self.cachedItems[key]
+        }
     }
     
     
